f8b826fa5ce20ad60cec881c2d6d51003f4a5439
Taisei Toya <tytaisei0102@gmail.com>
2021-06-28 13:24:56 +0200

    feat(index.vue): 都道府県リストを取得する処理を追加
    都道府県リストをasyncDataで取得する処理を追加
    pタグで仮表示する部分を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <Tutorial />
+  <p>{{ prefectures }}</p>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
+import { Context } from '@nuxt/types'
+import { fetchApiData } from '@/plugins/fetchApiData'
+import Prefecture, { createPrefectureArray } from '@/plugins/prefecture'
+
 @Component
-export default class index extends Vue {}
+export default class index extends Vue {
+  async asyncData(context: Context) {
+    const { $config } = context
+    let prefectures: Prefecture[] = []
+    const apiResponse = await fetchApiData(
+      'api/v1/prefectures',
+      $config.apiToken,
+      {}
+    )
+    if (apiResponse) {
+      prefectures = createPrefectureArray(apiResponse.data.result)
+    }
+    return { prefectures }
+  }
+}
 </script>

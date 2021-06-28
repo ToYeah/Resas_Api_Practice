f7b91605606bf6bd54d986aec62919f61794cff6
Taisei Toya <tytaisei0102@gmail.com>
2021-06-28 11:54:32 +0200

    feat(fetchApiData): Apiからデータを取得する関数を追加

---
+++ plugins/fetchApiData.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export const RESASAPI_ROOT = 'https://opendata.resas-portal.go.jp'
+
+export const fetchApiData = async (
+  requestEndPoint: string,
+  apiToken: string,
+  params: { [key: string]: string }
+): Promise<any | null> => {
+  const requestUri = new URL(requestEndPoint, RESASAPI_ROOT)
+  const response = await axios
+    .get(requestUri.toString(), {
+      headers: {
+        'X-API-KEY': apiToken,
+      },
+      params,
+    })
+    .catch(() => {
+      return null
+    })
+  return response
+}

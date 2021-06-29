1c335f9eeee2a3a21c7aa5678d544801208e156d
Taisei Toya <tytaisei0102@gmail.com>
2021-06-29 07:31:25 +0200

    feat(Chart.vue): グラフを表示するコンポーネントを仮作成

---
+++ components/Chart.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import Prefecture from '@/plugins/prefecture'
+
+@Component
+export default class CheckBox extends Vue {
+  public changePrefDisplay(pref: Prefecture) {
+    console.log(pref)
+  }
+}
+</script>

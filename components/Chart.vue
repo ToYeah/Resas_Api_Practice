d621de76d409f5a6d1011dc57537f0c0e1aee4e4
Taisei Toya <tytaisei0102@gmail.com>
2021-06-29 08:06:41 +0200

    feat(Chart.vue): 都道府県データの追加と削除を行う関数を追加
    changePrefDisplay関数にprefの真偽値による条件分けを追加
    都道府県名から都道府県の人口推移データを作成し、配列に追加する関数pushPrefDataを追加
    人口推移のリストから指定した都道府県名のデータを取り除く関数removePrefDataを追加

--- components/Chart.vue
+++ components/Chart.vue
@@ -1,15 +1,42 @@
 <template>
-  <div></div>
+  <div>{{ displayPrefData }}</div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import Prefecture from '@/plugins/prefecture'
+import PopulationTransition, {
+  createPopulationTransition,
+} from '~/plugins/populationTransition'
 
 @Component
 export default class CheckBox extends Vue {
+  private displayPrefData: PopulationTransition[] = []
+
   public changePrefDisplay(pref: Prefecture) {
-    console.log(pref)
+    if (pref.isDisplay) {
+      this.pushPrefData(pref)
+    } else {
+      this.removePrefData(pref.name)
+    }
+  }
+
+  private async pushPrefData(pref: Prefecture) {
+    const prefData = await createPopulationTransition(
+      this.$config.apiToken,
+      pref.name,
+      pref.id
+    )
+    if (prefData) {
+      this.displayPrefData.push(prefData)
+    }
+  }
+
+  private removePrefData(prefName: string) {
+    const index = this.displayPrefData.findIndex(
+      (elem) => elem.prefectureName === prefName
+    )
+    this.displayPrefData.splice(index, 1)
   }
 }
 </script>
